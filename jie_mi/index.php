42197b6c2fd2528d589b11a31602ee7529109571
jiemii <jie@cca.edu>
2023-04-23 23:53:17 -0700

    Module 10_update

---
+++ jie_mi/index.php
@@ -0,0 +1,20 @@
+<?php
+include_once "lib/php/functions.php";
+include_once "parts/templates.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Home</title>
+    <meta charset="UTF-8">
+
+    <?php include "parts/meta.php" ?>
+</head>
+
+<body>
+    <?php include "parts/navbar.php"; ?>
+    <h1>Home Page</h1>
+</body>
+
+</html>
